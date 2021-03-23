f9e184e4c6fe90c4923cde44bd5b775e64442289
zb-zc <zhaobo1997work@163.com>
2021-03-23 11:20:06 +0800

    Update TransformYUVandRGB.c

--- PictureProcess_NoteProject/TransformYUVandRGB.c
+++ PictureProcess_NoteProject/TransformYUVandRGB.c
@@ -1,16 +1,57 @@
 #include <stdio.h>
 int Sample_yuv420Separation(char *name,int h,int w)
 {
+	FILE *err;
 	FILE *fp = fopen(name,"rb+");
-	FILE *fp_y = fopen("ouotput420_y.y","wb+");
-	FILE *fp_u = fopen("ouotput420_u.y","wb+");
-	FILE *fp_v = fopen("ouotput420_v.y","wb+");
+	if(fp == NULL)
+	{
+		perror("fopen error!:");
+		exit(1);
+	}
+	FILE *fp_y = fopen("ouotput420_y.yuv","wb+");
+		if(fp_y == NULL)
+	{
+		perror("fopen fp_y error!:");
+		exit(1);
+	}
+	FILE *fp_u = fopen("ouotput420_u.yuv","wb+");
+		if(fp_u == NULL)
+	{
+		perror("fopen fp_u error!:");
+		exit(1);
+	}
+	FILE *fp_v = fopen("ouotput420_v.yuv","wb+");
+		if(fp_v == NULL)
+	{
+		perror("fopen fp_v error!:");
+		exit(1);
+	}
 
-	char *pic = (char *)malloc(w*h/3*2);
-	fread(pic,1,w*h/3*2,fp);
-	fwrite(pic,1,w*h,fp_y); // (w*h/3*2)
-	fwrite(pic+w*h,1,w*h/4,fp_u);
-	fwrite(pic+w*h*5/4,1,w*h/4,fp_v);
+	char *pic = (char *)malloc(w*h*3/2);//w*h(Y)+w*y*0.5(UV)= w*h*3/2
+	err = fread(pic,1,w*h*3/2,fp);
+	if(err == NULL)
+	{
+		perror("fread err:");
+		exit(1);
+	}
+	err = fwrite(pic,1,w*h,fp_y); // (w*h/3*2)
+	if(err == NULL)
+	{
+		perror("fwrite fp_y err:");
+		exit(1);
+	}
+	err = fwrite(pic+w*h,1,w*h/4,fp_u);
+	if(err == NULL)
+	{
+		perror("fwrite fp_u err:");
+		exit(1);
+	}
+	err = fwrite(pic+w*h*5/4,1,w*h/4,fp_v);
+	if(err == NULL)
+	{
+		perror("fwrite fp_v err:");
+		exit(1);
+	}
 	
 	free(pic);
 	fclose(fp);
@@ -21,10 +62,3 @@ int Sample_yuv420Separation(char *name,int h,int w)
 	return 0;
 }
 
-
-int main(int argc,char *argv)
-{
-printf("-----%s-----%d-----%d----\n",argv[1],argv[2],argv[3]);
-Sample_yuv420Separation(argv[1],(int)argv[2],(int)argv[3]);
-return 0;
-}
